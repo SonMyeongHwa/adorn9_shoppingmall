f690bc3d932bf2d38f901818cab44cdfe5efa7f9
dcmailreceiver@gmail.com <dcmailreceiver@gmail.com>
2023-10-05 19:33:44 +0900

    10051933

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,6 +2,8 @@ const {Router } = require ('express');
 const {User} =require ('../models');
 const asyncHandler = require('../utils/async-handler');
 const hashPassword = require('../utils/hash-password')
+// const telCheck = require('../services/telCheck')
+const checker = require('../services/usermodule')
 const router = Router();
 
 
@@ -14,10 +16,13 @@ router.post('/', asyncHandler(async (req,res)=>{
     console.log(req.body.createdAt)
     // const mailCheck = await User.findOne(email)
      const hashedPassword = hashPassword(password)//비밀번호 해쉬화
+
+    checker.telCheck(phone);
+    checker.mailformCheck(email)
+     if(id.length<3){throw new Error('아이디는 4글자 이상으로 설정해주세요.')}
     const mailcheck = await User.findOne({email})//중복 이메일 체크 변수
      if(mailcheck) {throw new Error('이미 가입된 이메일입니다.')} //중복 이메일 체크
      if(password.length<8) {throw new Error('비밀번호는 8글자 이상 입력해주세요')}//비번 자릿수 체크
-    //  if(id.length<8 )
      
    const user = await User.create({
       id,
@@ -28,34 +33,41 @@ router.post('/', asyncHandler(async (req,res)=>{
       email,
       // joindate
       })
-    res.send(user)
+    res.status(201).send({status:201,msg:`${name}님의 가입을 환영합니다.`,user})
     // res.redirect('/')
 }))
 
 //마이 프로필
 router.post('/profile' ,asyncHandler(async(req,res)=>{
   const viewkey = req.body.id
-  const Profile = await User.findOne({"id":viewkey})
-  res.send(userProfile)
+  const Profile = await User.findOne({id:viewkey})
+  res.status(200).send({status:200,Profile})
 }))
 
 //유저 수정
 router.put('/', asyncHandler(async(req,res)=>{
   
-  // const {id,password,name,address,phone,email} = req.body
-  const id = req.body.id
+  const {id,password,name,address,phone,email} = req.body
+  // const id = req.body.id
+  const mailcheck = await User.findOne({email})
+      if(mailcheck) {throw new Error('이미 존재하는 이메일입니다.')} //중복 이메일 체크
+     if(password.length<8) {throw new Error('비밀번호는 8글자 이상 입력해주세요')}
   User.findOneAndUpdate({id},{
+    password:hashPassword(req.body.password),
     name:req.body.name,
+    address:req.body.address,
+    phone:req.body.phone,
+    email:req.body.email,
     }).exec().then((updateUser)=>
-  res.send(updateUser))
+  res.status(200).send({status:200, msg:'개인정보가 수정됐습니다',updateUser}))
   
 }))
 
-//회원 탈퇴 미구현
+//회원 탈퇴
 router.delete('/',asyncHandler(async(req,res)=>{
   const deid = req.body.id
-  User.findOneAndDelete({id:deid})
-  res.send('ok')
+  await User.deleteOne({id:deid})
+  res.status(204).send({status:204, msg:'탈퇴가 완료됐습니다.'})
 
 }))
 
