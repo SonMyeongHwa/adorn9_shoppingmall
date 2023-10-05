564d78a32dafaeb6a552166cd32b34cc562abad1
dcmailreceiver@gmail.com <dcmailreceiver@gmail.com>
2023-10-05 09:12:07 +0900

    appcommit

--- server/app.js
+++ server/app.js
@@ -2,11 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose');
 const productRouter = require('./routes/productRouter');
-<<<<<<< HEAD
-const userRouter = require('./routes/userRouter')
-=======
 const orderRouter = require('./routes/orderRouter');
->>>>>>> 0f551eaac56389fb09b6e110b7ac913b167b3fbe
 require('dotenv').config();
 
 const MongoURL = process.env.MONGO_URL;
@@ -21,11 +17,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/products', productRouter);
-<<<<<<< HEAD
 app.use('/users', userRouter)
-=======
 app.use('/api/v1/orders', orderRouter);
->>>>>>> 0f551eaac56389fb09b6e110b7ac913b167b3fbe
 
 app.use('/', (req,res) => {
     res.send('oka');
