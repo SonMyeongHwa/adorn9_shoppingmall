0400c82a45f308aa75591f6c59bf2a015df519d6
ImGalim <dev.galim@gmail.com>
2023-10-06 20:48:26 +0900

    test

--- server/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-function errorHandler(error, req, res, next) {
-    res.status(400).json({ status: 400, msg: error.message });;
-  }
-  
-  module.exports = errorHandler;
-  
--- server/services/index.js
+++
@@ -1,5 +0,0 @@
-const { productService } = require('./productService');
-const { orderService } = require('./orderService');
-
-module.exports = { productService };
-module.exports = { orderService };
