b7d497d7435673624aa2c4fba195d138adf308ac
dcmailreceiver@gmail.com <dcmailreceiver@gmail.com>
2023-10-06 20:41:20 +0900

    merge

--- server/services/index.js
+++ server/services/index.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-const abc = {a:2,b:3,c:4}
-
-console.log(abc.a)
-=======
 const { productService } = require('./productService');
 const { orderService } = require('./orderService');
 
 module.exports = { productService };
 module.exports = { orderService };
->>>>>>> 387244b4993d38abaa4755bdb0a7ff935459a657
